d2f1415a39451a1f4bac54c836848c4c59376aff
vdespeisse <v.despeisse@gmail.com>
2016-10-16 20:02:02 +0200

    modif spam et workflow

--- spam.py
+++ spam.py
@@ -5,9 +5,7 @@ def spam(str):
 # attendre quelques secondes (le temps que tu click sur la window steamchat)
 #
 
-<<<<<<< HEAD
-if "caca proutt" == "__main__":
-=======
+
 if __name__ == "__main__":
->>>>>>> b9102f55627c1efd4599fea899c44556c9af37c1
+
     spam(sys.argv[1])
