8d167151ffe782b9bf44b0bbee613c879561f9e2
vdespeisse <v.despeisse@gmail.com>
2016-10-16 17:24:00 +0200

    base file

--- spam.py
+++ spam.py
@@ -1,1 +1,9 @@
-balblaba
+import sys
+
+def spam(str):
+    print str
+# attendre quelques secondes (le temps que tu click sur la window steamchat)
+#
+
+if __name__ == "__main__":
+    spam(sys.argv[1])
