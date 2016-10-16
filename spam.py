37459f102bd9f7cc9bfb361d9be68df084f27be7
firekoala <skyz3ra@gmail.com>
2016-10-16 22:38:34 +0200

    voilaaah

--- spam.py
+++ spam.py
@@ -1,11 +1,16 @@
 import sys
+import pyautogui
+import time
 
-def main():
+def main() :
 
+    pyautogui.PAUSE = 0.0001
+    time.sleep(2)
     list = ['a', 'b', 'c']
-    for i in ['a', 'b', 'c'] :
-        print(i)
-
+    for i in range(50) :
+        for i in ['test', 'spam', 'bot'] :
+            pyautogui.typewrite(i)
+            pyautogui.press('enter')
 # attendre quelques secondes (le temps que tu click sur la window steamchat)
 #
 main()
