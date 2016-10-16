9f330340a595cf70770680e7f231bc0bc3962e00
firekoala <skyz3ra@gmail.com>
2016-10-16 21:07:21 +0200

    123

--- spam.py
+++ spam.py
@@ -2,6 +2,10 @@ import sys
 
 def main(str):
     print (str)
+    list = ['a', 'b', 'c']
+    for i in  ['a', 'b', 'c'] :
+        print(i)
+
 # attendre quelques secondes (le temps que tu click sur la window steamchat)
 #
 main(sys.argv[1])
