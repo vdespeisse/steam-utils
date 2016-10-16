fefbd984795a342538e5235520455f4c3f82688f
vdespeisse <v.despeisse@gmail.com>
2016-10-16 21:09:34 +0200

    removed dumbshit

--- spam.py
+++ spam.py
@@ -1,11 +1,11 @@
 import sys
 
-def main(str):
-    print (str)
+def main():
+
     list = ['a', 'b', 'c']
-    for i in  ['a', 'b', 'c'] :
+    for i in ['a', 'b', 'c'] :
         print(i)
 
 # attendre quelques secondes (le temps que tu click sur la window steamchat)
 #
-main(sys.argv[1])
+main()
